a7f0e423753cee5a679bc61ed164b6fe0c3f6a3a
The End 7moz <k8668975@gmail.com>
2018-06-11 02:30:40 +0300

    First commit

--- header.php
+++ header.php
@@ -157,14 +157,14 @@
                                                id="confirm_password1"/>
                                     </label>
                                 </p>
-                                <p class="form-row form-row-wide margin-bottom-0">
+                                <p class="form-row form-row-wide ">
                                     <label for="condition" class="condition-label margin-bottom-0">
                                         <input type="checkbox" name="condition"
                                                id="condition"/>
                                         I hereby confirm i have one of the following tour guide qualifications-licence/certification/training.
                                     </label>
                                 </p>
-                                <p class="form-row form-row-wide margin-bottom-0">
+                                <p class="form-row form-row-wide ">
                                     <label for="condition2" class="condition-label">
                                         <input type="checkbox" name="condition2"
                                                id="condition2"/>
