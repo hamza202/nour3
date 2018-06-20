684f4d099b76d1267efbeb64383fc7b002c3eac2
The End 7moz <k8668975@gmail.com>
2018-06-21 01:59:15 +0300

    First commit

---
+++ Privacy.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <!-- Basic Page Needs
+    ================================================== -->
+    <title>Dgo</title>
+    <?php
+    include('style.php')
+    ?>
+
+
+</head>
+
+<body>
+
+<!-- Wrapper -->
+<div id="wrapper">
+
+    <!-- Header Container
+    ================================================== -->
+    <?php
+    include 'header.php'
+    ?>
+
+
+    <!-- Banner
+    ================================================== -->
+    <div class="clearfix"></div>
+    <div id="titlebar" class="gradient margin-bottom-35">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+
+                    <h2>FAQS</h2>
+
+                    <!-- Breadcrumbs -->
+                    <nav id="breadcrumbs">
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                            <li>Privacy Policy</li>
+                        </ul>
+                    </nav>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <section class="Privacy" >
+        <div class="container">
+            <div class="row">
+
+                <div class="col-md-12 faq margin-bottom-50">
+                    <h4 class="headline text-center margin-bottom-20">Our Privacy Policy</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus eligendi ex ipsam nesciunt nisi non numquam officiis quidem tempore vel. Alias aliquam debitis fugiat, perspiciatis placeat quaerat quam repellendus sint!</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur dicta doloremque necessitatibus repellat. Illo neque non reiciendis repellendus voluptates! Adipisci corporis cupiditate ex ipsam minus neque repudiandae soluta. Blanditiis, odio?</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus earum eos ipsa laborum natus omnis provident quia quis quisquam. Aliquam fugiat ipsum minima minus modi nesciunt nobis optio suscipit vel.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid atque beatae, consectetur ea illum in iusto maxime, molestias nobis repudiandae temporibus totam vel vitae? Corporis eaque iure odio quibusdam tempore!</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus eligendi ex ipsam nesciunt nisi non numquam officiis quidem tempore vel. Alias aliquam debitis fugiat, perspiciatis placeat quaerat quam repellendus sint!</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur dicta doloremque necessitatibus repellat. Illo neque non reiciendis repellendus voluptates! Adipisci corporis cupiditate ex ipsam minus neque repudiandae soluta. Blanditiis, odio?</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus earum eos ipsa laborum natus omnis provident quia quis quisquam. Aliquam fugiat ipsum minima minus modi nesciunt nobis optio suscipit vel.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid atque beatae, consectetur ea illum in iusto maxime, molestias nobis repudiandae temporibus totam vel vitae? Corporis eaque iure odio quibusdam tempore!</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <!-- Footer
+    ================================================== -->
+    <?php
+    include 'footer.php'
+    ?>
+    <!-- Footer / End -->
+
+
+    <!-- Back To Top Button -->
+    <div id="backtotop"><a href="#"></a></div>
+
+
+</div>
+<!-- Wrapper / End -->
+
+
+<?php
+include "javascript.php";
+?>
+
+</body>
+
+</html>
