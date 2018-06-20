063a8397b8e5ec3a41129b5e898828c5a936587d
The End 7moz <k8668975@gmail.com>
2018-06-21 01:31:47 +0300

    First commit

---
+++ faq.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <!-- Basic Page Needs
+    ================================================== -->
+    <title>Dgo</title>
+    <?php
+    include('style.php')
+    ?>
+
+
+</head>
+
+<body>
+
+<!-- Wrapper -->
+<div id="wrapper">
+
+    <!-- Header Container
+    ================================================== -->
+    <?php
+    include 'header.php'
+    ?>
+
+
+    <!-- Banner
+    ================================================== -->
+    <div class="clearfix"></div>
+    <div id="titlebar" class="gradient margin-bottom-35">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+
+                    <h2>FAQS</h2>
+
+                    <!-- Breadcrumbs -->
+                    <nav id="breadcrumbs">
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                            <li>FAQS</li>
+                        </ul>
+                    </nav>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <section class="faq" >
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2 margin-bottom-20">
+                    <p class="text-center ">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aspernatur consequatur culpa deleniti, dolorem ea explicab
+                    </p>
+                </div>
+                <div class="col-md-6 faq margin-bottom-50">
+                    <div class="text-center">
+                    <h4 class="headline text-center margin-bottom-20">Tourist FAQS</h4>
+                    </div>
+                    <!-- Toggles Container -->
+                    <div class="style-2">
+
+                        <!-- Toggle 1 -->
+                        <div class="toggle-wrap">
+                            <span class="trigger "><a href="#">First Toggle<i class="sl sl-icon-plus"></i></a></span>
+                            <div class="toggle-container">
+                                <p>Perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam. Donec ut volutpat metus. Vivamus justo arcu, elementum a sollicitudin pellentesque, tincidunt ac enim. Proin id arcu ut ipsum vestibulum elementum.</p>
+                            </div>
+                        </div>
+
+                        <!-- Toggle 2 -->
+                        <div class="toggle-wrap">
+                            <span class="trigger"><a href="#">Second Toggle With Icon <i class="sl sl-icon-plus"></i> </a></span>
+                            <div class="toggle-container">
+                                <p>Seded ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam. Donec ut volutpat metus. Aliquam tortor lorem, fringilla tempor dignissim at, pretium et arcu.</p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="col-md-6 faq margin-bottom-50">
+                    <div class="text-center">
+                    <h4 class="headline text-center margin-bottom-20">Tourist leader FAQS</h4>
+                    </div>
+                    <!-- Toggles Container -->
+                    <div class="style-2">
+
+                        <!-- Toggle 1 -->
+                        <div class="toggle-wrap">
+                            <span class="trigger "><a href="#">First Toggle<i class="sl sl-icon-plus"></i></a></span>
+                            <div class="toggle-container">
+                                <p>Perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam. Donec ut volutpat metus. Vivamus justo arcu, elementum a sollicitudin pellentesque, tincidunt ac enim. Proin id arcu ut ipsum vestibulum elementum.</p>
+                            </div>
+                        </div>
+
+                        <!-- Toggle 2 -->
+                        <div class="toggle-wrap">
+                            <span class="trigger"><a href="#">Second Toggle With Icon <i class="sl sl-icon-plus"></i> </a></span>
+                            <div class="toggle-container">
+                                <p>Seded ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam. Donec ut volutpat metus. Aliquam tortor lorem, fringilla tempor dignissim at, pretium et arcu.</p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <!-- Footer
+    ================================================== -->
+    <?php
+    include 'footer.php'
+    ?>
+    <!-- Footer / End -->
+
+
+    <!-- Back To Top Button -->
+    <div id="backtotop"><a href="#"></a></div>
+
+
+</div>
+<!-- Wrapper / End -->
+
+
+<?php
+include "javascript.php";
+?>
+
+</body>
+
+</html>
